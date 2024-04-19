5d2fad03a058dfd27cdae72c763f0362a14392f6
KhoiBuiSMU27 <167252039+KhoiBuiSMU27@users.noreply.github.com>
2024-04-19 13:34:11 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <ItemToPurchase.h>
 using namespace std;
-
-// update from user-1
 
 int main(){
 
+  // update from Kevin
+
+  ItemToPurchase item1, item2;
+  string name;
+  int price, quantity;
+
+  cout << "Item 1" << endl;
+  cout << "Enter the item name:" << endl;;
+  getline(cin, name);
+  cout << "Enter the item price:" << endl;
+  cin >> price;
+  cout << "Enter the item quantity:" << endl;
+  cin >> quantity;
+
+  item1.SetName(name);
+  item1.SetPrice(price);
+  item1.SetQuantity(quantity);
+
+  cin.ignore();
   
   return 0;
   
