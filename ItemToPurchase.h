a3b1e8741f06a19712250ad8e1ee592cc1b71653
KhoiBuiSMU27 <167252039+KhoiBuiSMU27@users.noreply.github.com>
2024-04-19 13:25:13 -0500

    Update ItemToPurchase.h

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -1,1 +1,23 @@
+#include <string>
+using namespace std;
 
+class ItemToPurchase {
+
+    private:
+    string itemName;
+    int itemPrice;
+    int itemQuanity;
+
+    public:
+    ItemToPurchase();
+
+    void SetName(string name);
+    string GetName();
+
+    void SetPrice(int price);
+    int GetPrice();
+
+    void SetQuantity(int quantity);
+    int GetQuantity();
+
+};
