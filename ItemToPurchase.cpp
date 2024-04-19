fb73d4baa50dd85f2b6ebe5666cf2eff9eaba866
KhoiBuiSMU27 <167252039+KhoiBuiSMU27@users.noreply.github.com>
2024-04-19 13:25:52 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include "ItemToPurchase.h"
+
+using namespace std;
+
+ItemToPurchase::ItemToPurchase(){
+
+    itemName = "none";
+    itemPrice = 0;
+    itemQuanity = 0;
+
+}
+
+void ItemToPurchase::SetName(string name){
+    itemName = name;
+}
+
+string ItemToPurchase::GetName(){
+    return itemName;
+}
+
+void ItemToPurchase::SetPrice(int price){
+    itemPrice = price;
+}
+
+int ItemToPurchase::GetPrice(){
+    return itemPrice;
+}
+
+void ItemToPurchase::SetQuantity(int quantity){
+    itemQuanity = quantity;
+}
+
+int ItemToPurchase::GetQuantity(){
+    return itemQuanity;
+}
+
 
