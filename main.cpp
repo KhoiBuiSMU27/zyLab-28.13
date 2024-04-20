fd3564fe382a51c7d537600e71f042797e2e2fc1
KhoiBuiSMU27 <167252039+KhoiBuiSMU27@users.noreply.github.com>
2024-04-20 07:51:27 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,56 +1,44 @@
 #include <iostream>
+#include <ItemToPurchase.h>
 using namespace std;
-
-// update from user-1
 
 int main(){
 
- ItemToPurchase item1, item2;
-int price, quantity;
-string name;
-  
-cout << "Item 1" << endl;
-cout << "Enter the item name:" << endl;
-getline(cin, name);
-item1.SetName(name);
-//cout << name << endl;
-
-cout << "Enter the item price:" << endl;
-cin >> price;
-item1.SetPrice(price);
-//cout << price << endl;
-
-cout << "Enter the item quantity:" << endl;
-cin >> quantity;
-item1.SetQuantity(quantity);
-//cout << quantity << endl;
-cout << endl;
-
-cin.ignore();
-
-cout << "Item 2" << endl;
-cout << "Enter the item name:" << endl;
-getline(cin, name);
-item1.SetName(name);
-//cout << name << endl;
-
-cout << "Enter the item price:" << endl;
-cin >> price;
-item1.SetPrice(price);
-//cout << price << endl;
-
-cout << "Enter the item quantity:" << endl;
-cin >> quantity;
-item1.SetQuantity(quantity);
-//cout << quantity << endl;
-
-cout << endl;
-
-
-
-
-  
-  
+	ItemToPurchase item1, item2;
+	int price, quantity;
+	string name;
+	
+	cout << "Item 1" << endl;
+	cout << "Enter the item name:" << endl;
+	getline(cin, name);
+	item1.SetName(name);
+ 
+	cout << "Enter the item price:" << endl;
+	cin >> price;
+	item1.SetPrice(price);
+	
+	cout << "Enter the item quantity:" << endl;
+	cin >> quantity;
+	item1.SetQuantity(quantity);
+	cout << endl;
+	
+	cin.ignore();
+	
+	cout << "Item 2" << endl;
+	cout << "Enter the item name:" << endl;
+	getline(cin, name);
+	item2.SetName(name);
+	
+	cout << "Enter the item price:" << endl;
+	cin >> price;
+	item2.SetPrice(price);
+	
+	cout << "Enter the item quantity:" << endl;
+	cin >> quantity;
+	item2.SetQuantity(quantity);
+	
+	cout << endl;
+	
   return 0;
   
 }
