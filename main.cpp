aa80e4ab776ee0485baeb27fe41622cb1497b528
muxinge <167571367+muxinge@users.noreply.github.com>
2024-04-19 22:40:25 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,18 @@ using namespace std;
 
 int main(){
 
+ItemToPurcahse item1, item2;
+int price, quantity;
+string name;
+  
+cout << "Item 1" << endl;
+cout << "Enter the item name:" << endl;
+getline(cin, name);
+Item1.setname(name);
+cout << "Enter the item price:" << endl;
+
+
+  
   
   return 0;
   
