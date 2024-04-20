35044d3d43fdae9061990bf4db217e80c4005955
muxinge <167571367+muxinge@users.noreply.github.com>
2024-04-19 22:43:32 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,8 +12,38 @@ string name;
 cout << "Item 1" << endl;
 cout << "Enter the item name:" << endl;
 getline(cin, name);
-Item1.setname(name);
+item1.SetName(name);
+cout << name << endl;
+
 cout << "Enter the item price:" << endl;
+cin >> price;
+item1.SetPrice(price);
+cout << price << endl;
+
+cout << "Enter the item quantity: ";
+cin >> quantity;
+item1.SetQuantity(quantity);
+cout << quantity << endl;
+
+cin.ignore();
+
+cout << "Item 2" << endl;
+cout << "Enter the item name:" << endl;
+getline(cin, name);
+item1.SetName(name);
+cout << name << endl;
+
+cout << "Enter the item price:" << endl;
+cin >> price;
+item1.SetPrice(price);
+cout << price << endl;
+
+cout << "Enter the item quantity: ";
+cin >> quantity;
+item1.SetQuantity(quantity);
+cout << quantity << endl;
+
+
 
 
   
